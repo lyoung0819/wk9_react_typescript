d1d6fab0dc5c90d5f3f7a598e4af33d677c99c58
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-15 14:30:57 -0500

    function to return an html element

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+
 export default function App(){
-  return <h1>Hello World</h1>
+  // return React.createElement('div', {}, 'Hello World') // (element, props, element children(can be another createElement)) // would require import Reach from 'react';
+  // return <h1>Hello World!!!!!</h1> // this html-looking text is actually JSX (or TSX since we're using typescript). It is creating a React element
+
+  // within the TSX/JSX, we can hard code out of it to normal TS/JS by using {}
+  const firstName: string = 'Lexie';
+  const lastName: string = 'Young';
+  const isLoggedIn: boolean = false;
+  const posts: {id:number, title:string}[] = [
+    {id: 1, title: 'Happy Monday'},
+    {id: 2, title: 'React Rules'},
+    {id: 3, title: 'Spring has Sprung'}
+  ]
+
+  return (
+    <div>
+        <h1>Hello World</h1>
+        <h2>{!isLoggedIn ? `Welcome back, ${firstName}` : 'Please Log In or Sign Up'}</h2>
+        {posts.map( p => <h4>{p.title}</h4> )}
+    </div>
+  )
 }
