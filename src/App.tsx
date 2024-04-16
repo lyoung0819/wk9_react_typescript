628f2e4e09840267cfdfaa136499accfd7a1c27f
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-16 12:17:04 -0500

    added stateful event handlers for sorting by option values in asc/dsc way and added a search bar to filter through titles of posts with given search

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Navigation from "./components/Navigation";
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 export default function App() {
@@ -19,6 +21,9 @@ export default function App() {
   //   {id: 2, title: 'React Rules'},
   //   {id: 3, title: 'Spring has Sprung'}
   // ]
+
+
+  // Types 
   type Post = {
     id: number,
     title: string
@@ -26,35 +31,45 @@ export default function App() {
 
   type Sorting = {
     idAsc: (a: Post, b:Post) => number,
-    idDesc: (a: Post, b:Post) => number,
+    idDsc: (a: Post, b:Post) => number,
     titleAsc: (a: Post, b:Post) => number,
-    titleDesc: (a: Post, b:Post) => number,
+    titleDsc: (a: Post, b:Post) => number,
   }
 
-
+  // States
   const [posts, setPosts] = useState([
     { id: 1, title: 'Happy Monday' },
     { id: 2, title: 'React Rules' },
-    { id: 3, title: 'Spring has Sprung' }
+    { id: 3, title: 'Spring has Sprung' },
+    {id: 4, title: 'Another Post'},
+    {id: 5, title: 'Lovely Tuesday today!'}
   ]);
 
+  const [searchTerm, setSearchTerm] = useState(''); // initially will be empty string because there will be no words in search bar
+
+  // Handler Events
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    
-    const sortFunctions: Sorting = {
-      idAsc: (a: Post, b: Post) => a.id - b.id,
-      idDesc: (a: Post, b: Post) => b.id - a.id,
-      titleAsc: (a: Post, b: Post) => a.title > b.title ? 1 : -1,
-      titleDesc: (a: Post, b: Post) => b.title > a.title ? 1 : -1
-    }
+      const sortFunctions: Sorting = {
+        idAsc: (a: Post, b: Post) => a.id - b.id,
+        idDsc: (a: Post, b: Post) => b.id - a.id,
+        titleAsc: (a: Post, b: Post) => a.title > b.title ? 1 : -1,
+        titleDsc: (a: Post, b: Post) => b.title > a.title ? 1 : -1
+      }
     let func = sortFunctions[e.target.value as keyof Sorting]
     let newSortedArr = [...posts].sort(func)
     setPosts(newSortedArr)
   }
-
+  
   const handleClick = () => {
     setisLoggedIn(!isLoggedIn)
   }
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
+  }
+  
+
+  // Return
   return (
     <>
       <Navigation isLoggedIn={isLoggedIn} />
@@ -62,14 +77,22 @@ export default function App() {
         <h1>Hello World</h1>
         <Button variant='primary' onClick={handleClick}>Click me!</Button>
         <h2>{isLoggedIn ? `Welcome back, ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
-        {isLoggedIn && <Form.Select onChange={handleSelectChange}>
-          <option value='main'>Sort Posts by ID or Title:</option>
-          <option value='idAsc'>Sort by ID Ascending</option>
-          <option value='idDsc'>Sort by ID Descending</option>
-          <option value='titleAsc'>Sort by Title Ascending</option>
-          <option value='titleDsc'>Sort by Title Descending</option>
-        </Form.Select>}
-        {isLoggedIn && posts.map(p => <h4 key={p.id}>{p.title}</h4>)}
+        <Row>
+          <Col xs={12} md={8}>
+              <Form.Control value={searchTerm} onChange={handleInputChange} placeholder="Search Posts" />
+          </Col>
+          <Col>
+              <Form.Select onChange={handleSelectChange}>
+                  <option value='main'>Sort Posts by ID or Title:</option>
+                  <option value='idAsc'>Sort by ID Ascending</option>
+                  <option value='idDsc'>Sort by ID Descending</option>
+                  <option value='titleAsc'>Sort by Title Ascending</option>
+                  <option value='titleDsc'>Sort by Title Descending</option>
+              </Form.Select>
+          </Col>
+        </Row>
+        
+        {isLoggedIn && posts.filter( p => p.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())).map(p => <h4 key={p.id}>{p.title}</h4>)}
       </Container>
     </>
   )
